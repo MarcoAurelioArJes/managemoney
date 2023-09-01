ccb74496d325e2a98dfae2e500e7d4640d8e66a0
marco.jesus <marco.jesus@inventsoftware.com.br>
2023-09-01 11:55:12 -0300

    ConnectionString arrumada

--- Program.cs
+++ Program.cs
@@ -9,11 +9,11 @@ using managemoney.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//var stringDeConexao = OperatingSystem.IsLinux() ? builder.Configuration.GetConnectionString("Default") 
-//                                                    : builder.Configuration.GetConnectionString("Local");
+var stringDeConexao = OperatingSystem.IsLinux() ? builder.Configuration.GetConnectionString("Default") 
+                                                    : builder.Configuration.GetConnectionString("Local");
 
 builder.Services.AddDbContext<ApplicationContext>(o => {
-    o.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoAZ"));
+    o.UseSqlServer(stringDeConexao);
 });
 
 builder
